cc7225f6ecc19cba6aba3074c910f663e13d811a
fimars <foru.fy@gmail.com>
2023-11-21 00:33:13 +0800

    feat: updat favicon, site info

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'nichijo - pinkin';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'nichijo -> riki-chan';
+export const SITE_DESCRIPTION = 'fimars\'s blog, tracking life.';
